eed95a071ef9eee08901b1af1cec32938f87b8c1
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2025-09-29 11:19:55 -0400

    kill 0

--- src/unity-hub.ts
+++ src/unity-hub.ts
@@ -125,7 +125,7 @@ export class UnityHub {
                         if (outputLine.includes(tasksComplete)) {
                             if (child?.pid) {
                                 Logger.instance.warn(`Unity Hub reported all tasks completed, terminating process...`);
-                                process.kill(child.pid, 'SIGTERM');
+                                process.kill(child.pid, 0);
                             }
                         }
                     } catch (error: any) {
