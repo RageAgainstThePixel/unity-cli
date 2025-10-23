7bdb473482416b4af5048a1df5b0f2467523a9e9
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2025-10-23 17:47:33 -0400

    tweak

--- src/unity-hub.ts
+++ src/unity-hub.ts
@@ -406,7 +406,7 @@ sudo apt-get install -y --no-install-recommends --only-upgrade unityhub`]);
             }
             case 'linux': {
                 await Exec('sudo', ['sh', '-c', `#!/bin/bash
-set -xe -o pipefail
+set -xe
 dbus-uuidgen >/etc/machine-id && mkdir -p /var/lib/dbus/ && ln -sf /etc/machine-id /var/lib/dbus/machine-id
 wget -qO - https://hub.unity3d.com/linux/keys/public | gpg --dearmor | tee /usr/share/keyrings/Unity_Technologies_ApS.gpg >/dev/null
 echo "deb [signed-by=/usr/share/keyrings/Unity_Technologies_ApS.gpg] https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list
