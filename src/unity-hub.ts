66e45a921035ce3cd7a1eb50433ddc0504cd365b
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2025-10-23 17:30:26 -0400

    show process

--- src/unity-hub.ts
+++ src/unity-hub.ts
@@ -406,12 +406,12 @@ sudo apt-get install -y --no-install-recommends --only-upgrade unityhub`]);
             }
             case 'linux': {
                 await Exec('sudo', ['sh', '-c', `#!/bin/bash
-set -e
+set -xe -o pipefail
 dbus-uuidgen >/etc/machine-id && mkdir -p /var/lib/dbus/ && ln -sf /etc/machine-id /var/lib/dbus/machine-id
 wget -qO - https://hub.unity3d.com/linux/keys/public | gpg --dearmor | tee /usr/share/keyrings/Unity_Technologies_ApS.gpg >/dev/null
 echo "deb [signed-by=/usr/share/keyrings/Unity_Technologies_ApS.gpg] https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list
 echo "deb https://archive.ubuntu.com/ubuntu jammy main universe" | tee /etc/apt/sources.list.d/jammy.list
-apt-get update
+sudo apt-get update --allow-releaseinfo-change
 apt-get install -y --no-install-recommends unityhub ffmpeg libgtk2.0-0 libglu1-mesa libgconf-2-4 libncurses5
 apt-get clean
 sed -i 's/^\\(.*DISPLAY=:.*XAUTHORITY=.*\\)\\( "\\$@" \\)2>&1$/\\1\\2/' /usr/bin/xvfb-run
