2d17726b99b121ae9636fcc56343e2292d21c61a
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2025-10-28 11:47:02 -0400

    revert linux installer upgrade

--- src/unity-hub.ts
+++ src/unity-hub.ts
@@ -332,7 +332,12 @@ export class UnityHub {
                     await DeleteDirectory(this.rootDirectory);
                     await this.installHub(version);
                 } else if (process.platform === 'linux') {
-                    await this.installHub(version);
+                    await Exec('sudo', ['sh', '-c', `#!/bin/bash
+set -e
+wget -qO - https://hub.unity3d.com/linux/keys/public | gpg --dearmor | sudo tee /usr/share/keyrings/Unity_Technologies_ApS.gpg >/dev/null
+sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/Unity_Technologies_ApS.gpg] https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
+sudo apt-get update --allow-releaseinfo-change
+sudo apt-get install -y --no-install-recommends --only-upgrade unityhub${version ? '=' + version : ''}`]);
                 } else {
                     throw new Error(`Unsupported platform: ${process.platform}`);
                 }
