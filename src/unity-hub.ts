66f1dc6f631d64157b6e4bfe47a737b3f4773299
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2025-11-26 11:17:08 -0500

    breakup the linux package install line to make it a bit easier to read

--- src/unity-hub.ts
+++ src/unity-hub.ts
@@ -446,7 +446,15 @@ wget -qO - https://hub.unity3d.com/linux/keys/public | gpg --dearmor | tee /usr/
 echo "deb [signed-by=/usr/share/keyrings/Unity_Technologies_ApS.gpg] https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list
 echo "deb https://archive.ubuntu.com/ubuntu jammy main universe" | tee /etc/apt/sources.list.d/jammy.list
 apt-get update
-apt-get install -y --no-install-recommends unityhub${version ? '=' + version : ''} ffmpeg libgtk2.0-0 libglu1-mesa libgconf-2-4 libncurses5 pulseaudio
+apt-get install -y --no-install-recommends \\
+  unityhub${version ? '=' + version : ''} \\
+  xvfb \\
+  ffmpeg \\
+  libgtk2.0-0 \\
+  libglu1-mesa \\
+  libgconf-2-4 \\
+  libncurses5 \\
+  pulseaudio
 apt-get clean
 sed -i 's/^\\(.*DISPLAY=:.*XAUTHORITY=.*\\)\\( "\\$@" \\)2>&1$/\\1\\2/' /usr/bin/xvfb-run
 printf '#!/bin/bash\nxvfb-run --auto-servernum /opt/unityhub/unityhub "$@" 2>/dev/null' | tee /usr/bin/unity-hub >/dev/null
